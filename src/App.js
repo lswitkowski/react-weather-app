817c8bffa6cb75f65aa4e56858de89be055b73d9
lswitkowski <63552929+lswitkowski@users.noreply.github.com>
2024-11-05 14:19:23 -0800

    Footer

--- src/App.js
+++ src/App.js
@@ -3,11 +3,30 @@ import Weather from "./Weather.js";
 
 function App() {
   return (
- 
-   <div className="App">
+    <div className="App">
       <h1>Current Weather</h1>
       <h2>Anywhere in the world!</h2>
-       <Weather/>
+      <Weather />
+      <p>
+        This website was coded by
+        <a
+          href="https://github.com/lswitkowski/"
+          target="_blank">
+          {" "}
+          Lisa W
+        </a>
+        , open-sourced on{" "}
+        <a
+          href="https://github.com/lswitkowski?tab=repositories"
+          target="_blank"
+        >
+          GitHub
+        </a>
+        , and hosted on
+        <a href="https://www.netlify.com/" target="_blank" title="netlify">
+          Netlify
+        </a>
+      </p>
     </div>
   );
 }
