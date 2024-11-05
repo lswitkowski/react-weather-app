68bfcaad317b6c19c5694b6a45013dbed2796e3a
lswitkowski <63552929+lswitkowski@users.noreply.github.com>
2024-11-05 13:26:34 -0800

    Basic structure /Axios/ Api call

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
+import axios from "axios";
 import './App.css';
+import Weather from "./Weather.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Current Weather</h1>
+      <h2>Anywhere in the world!</h2>
+       <Weather/>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,64 @@
+import React, {useState} from "react";
+import axios from "axios";
+import "./App.css";
+
+export default function Weather(){
+      let [city, setCity] = useState("");
+      let [message, setMessage] = useState("");
+      let [weather, setWeather] = useState("");
+     
+    function updateCity(event) {
+        setCity(event.target.value);
+      }
+    
+        function displayWeather(response) {
+          setMessage(true);
+          setWeather({
+            temperature: response.data.main.temp,
+            wind: response.data.wind.speed,
+            humidity: response.data.main.humidity,
+            icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+            description: response.data.weather[0].description,
+          });
+        }
+        function handleSubmit(event) {
+          event.preventDefault();
+          let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+          let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+          axios.get(apiUrl).then(displayWeather);
+        }
+        let form = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="Enter a city"
+        autofocus
+        required
+        onChange={updateCity}
+      />
+      <input type="submit" value="Search" />
+    </form>
+  );
+
+  if (message) {
+    return (
+      <div>
+        {form}
+        <h2 className="city">{city}</h2>
+        <p>
+          Temperature: {Math.round(weather.temperature)}°C
+          <br />
+          Description: {weather.description}
+          <br />
+          Humidity: {weather.humidity}%<br />
+          Wind: {weather.wind}km/h
+          <br />
+          <img src={weather.icon} alt={weather.description} />
+          <br />
+        </p>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
