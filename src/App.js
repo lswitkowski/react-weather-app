cb8f2eb4ec13964e90647c7d5e8d46ac74dc1071
lswitkowski <63552929+lswitkowski@users.noreply.github.com>
2024-11-08 19:45:48 -0800

    Css

--- src/App.js
+++ src/App.js
@@ -4,37 +4,40 @@ import Weather from "./Weather.js";
 function App() {
   return (
     <div className="App">
-      <h1>Current Weather</h1>
-      <h2>Anywhere in the world!</h2>
-      <Weather />
-      <p>
-        This website was coded by
-        <a
-          href="https://github.com/lswitkowski/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          Lisa W
-        </a>
-        , open-sourced on{" "}
-        <a
-          href="https://github.com/lswitkowski?tab=repositories"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-        , and hosted on
-        <a
-          href="https://www.netlify.com/"
-          target="_blank"
-          rel="noreferrer"
-          title="netlify"
-        >
-          Netlify
-        </a>
-      </p>
+      <div className="Weather-Container">
+        <h1 class="p-1">Current Weather</h1>
+        <h2>Anywhere in the world!</h2>
+        <Weather />
+        <p>
+          <footer>
+            This website was coded by{" "}
+            <a
+              href="https://github.com/lswitkowski/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Lisa W
+            </a>
+            , open-sourced on{" "}
+            <a
+              href="https://github.com/lswitkowski?tab=repositories"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>
+            , and hosted{" "}
+            <a
+              href="https://www.netlify.com/"
+              target="_blank"
+              rel="noreferrer"
+              title="netlify"
+            >
+              Netlify
+            </a>
+          </footer>
+        </p>
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,62 +1,80 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
-import "./App.css";
+import "./Weather.css";
 
-export default function Weather(){
-      let [city, setCity] = useState("");
-      let [message, setMessage] = useState("");
-      let [weather, setWeather] = useState("");
-     
-    function updateCity(event) {
-        setCity(event.target.value);
-      }
-    
-        function displayWeather(response) {
-          setMessage(true);
-          setWeather({
-            temperature: response.data.main.temp,
-            wind: response.data.wind.speed,
-            humidity: response.data.main.humidity,
-            icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
-            description: response.data.weather[0].description,
-          });
-        }
-        function handleSubmit(event) {
-            event.preventDefault();
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
-          let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-          let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-          axios.get(apiUrl).then(displayWeather);
-        }
-        let form = (
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.coord,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+  let form = (
     <form onSubmit={handleSubmit}>
       <input
+        className="Input-Field"
         type="search"
         placeholder="Enter a city"
-        autofocus
+        autoFocus
         required
         onChange={updateCity}
       />
-      <input type="submit" value="Search" />
+      <input className="Button" type="submit" value="Search" />
     </form>
   );
 
-  if (message) {
+  if (weatherData.ready) {
     return (
-      <div>
-        {form}
-        <h2 className="city">{city}</h2>
-        <p>
-          Temperature: {Math.round(weather.temperature)}°C
-          <br />
-          Description: {weather.description}
-          <br />
-          Humidity: {weather.humidity}%<br />
-          Wind: {weather.wind}km/h
-          <br />
-          <img src={weather.icon} alt={weather.description} />
-          <br />
-        </p>
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <WeatherInfo data={weatherData} />
+        <WeatherForecast coordinates={weatherData.coordinates} />
       </div>
     );
   } else {
