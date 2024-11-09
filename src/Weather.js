3a0a4dac1188d2df57b04685ff6c793f16fad934
lswitkowski <63552929+lswitkowski@users.noreply.github.com>
2024-11-08 20:20:20 -0800

    Add formatting functions

--- src/Weather.js
+++ src/Weather.js
@@ -34,8 +34,9 @@ export default function Weather(props) {
   function search() {
     let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayWeather);
+     axios.get(apiUrl).then(handleResponse);
   }
+  
   let form = (
     <form onSubmit={handleSubmit}>
       <input
@@ -44,7 +45,7 @@ export default function Weather(props) {
         placeholder="Enter a city"
         autoFocus
         required
-        onChange={updateCity}
+        onChange={handleCityChange}
       />
       <input className="Button" type="submit" value="Search" />
     </form>
@@ -79,5 +80,5 @@ export default function Weather(props) {
     );
   } else {
     return form;
-  }
-}
+  }}
+
